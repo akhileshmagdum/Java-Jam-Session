b71de21952890d947826d8e7928a406b934ae5a2
akhileshmagdum <akhileshmagdum@gmail.com>
2024-05-19 22:25:51 +0530

    Creating custom lambda

---
+++ src/learn/core/funcinterfaced/ClassFIDemo.java
@@ -0,0 +1,9 @@
+package learn.core.funcinterfaced;
+
+public class ClassFIDemo {
+
+    public static void main(String[] args) {
+        FunctionalInterfaceDemo interfaceDemo = () -> System.out.println("Akhilesh");
+        interfaceDemo.print();
+    }
+}

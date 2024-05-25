ec7dbd25b39e61ea62ea82939a4f4b0f42b252f9
akhileshmagdum <akhileshmagdum@gmail.com>
2024-05-25 14:47:51 +0530

    String tricks

--- src/learn/core/GeneralTricks.java
+++ src/learn/core/GeneralTricks.java
@@ -7,10 +7,14 @@ public class GeneralTricks {
      * The static and public's position can be interchanged
      */
     static public void main(String[] args) {
-        System.out.println(100 + 20 + "Result");
-        System.out.println("100" + 20 + "Result");
-        System.out.println(100 + "Result" + 20);
-        System.out.println("100" + 20);
+        System.out.println(100 + 20 + "Result"); // 130Result
+        System.out.println("100" + 20 + "Result"); // 10020Result
+        System.out.println(100 + "Result" + 20); // 100Result20
+        System.out.println("100" + 20 + 10); // 1002010
+        System.out.println(new String("Akhilesh") == new String("Akhilesh")); // false
+        System.out.println(new String("Akhilesh") == "Akhilesh"); //false
+        System.out.println("Akhilesh" == "Akhilesh"); //true
+        System.out.println(new String("Akhilesh").intern() == "Akhilesh"); //true
     }
 
     /**
